15eb1a0ee319cef73d5c6a50dc111a70019e42aa
Thokora <fawergilthokora@hotmail.com>
2018-04-29 23:34:29 -0500

    29/04/2018
    Baked

--- LaberintoSensorial/Assets/Scripts/PistaScript.cs
+++ LaberintoSensorial/Assets/Scripts/PistaScript.cs
@@ -7,13 +7,15 @@ public class PistaScript : MonoBehaviour {
     Movimiento movScript;
     public GameObject pista;
     public GameObject player;
+    public GameObject[] Pistas;
     float ValorRot;
 
-	void Start () {
-		
-	}
-	
-	void Update () {
+    void Start()
+    {
+
+    }
+
+    void Update () {
 
         pista.transform.position = 
             new Vector3(player.transform.position.x, player.transform.position.y, (player.transform.position.z));
@@ -21,5 +23,10 @@ public class PistaScript : MonoBehaviour {
         ValorRot = player.transform.eulerAngles.y;
         pista.transform.eulerAngles = new Vector3(0, ValorRot, 0);
 
+    }
+    public void MostrarPistas()
+    {
+        Pistas[0].SetActive(false);
+        Pistas[1].SetActive(true);
     }
 }
